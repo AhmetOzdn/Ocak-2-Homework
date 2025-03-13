aa9e07f2217a3c6eb85d3a1b736973e7c7d7ca12
Ahmet Özden <a.ozdenn0@gmail.com>
2025-03-13 14:01:34 +0300

    Ocak-2.Hafta Ödev

---
+++ Ocak-2-Homework/Ocak-2-Homework/Program.cs
@@ -0,0 +1,9 @@
+﻿// Kullanıcıdan iki sayı alıp toplamını ekrana yazdıran bir program yazın.
+
+Console.WriteLine("Konsola yazmak istediğiniz sayıları sırayla giriniz : ");
+
+int userNumberOne = Convert.ToInt32(Console.ReadLine());
+int userNumberTwo = Convert.ToInt32(Console.ReadLine());
+int sum = userNumberOne + userNumberTwo;
+
+Console.WriteLine($"Yazdığınız sayıların toplamı : {sum}");
